d6fc752bd4d6888e0401c7d6f95643936e13facc
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-05-10 23:05:26 -0300

    feat: Implementando rotas

--- src/index.js
+++ src/index.js
@@ -1,25 +1,64 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+function checksUsername(request, response, next) {
+  const { username } = request.body;
+  const user = users.find((user) => user.username === username);
+
+  if (user){
+    return response.status(400).json({ error: 'User already exists' });
+  }
+
+  return next();
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+
+  if (!user){
+    return response.status(400).json({ error: 'User does not exists' });
+  }
+
+  request.user = user;
+
+  return next();
+}
+
+app.post('/users',checksUsername, (request, response) => {
+  const { name, username } = request.body;
+
+  const id = uuidv4();
+
+  users.push({
+    id,
+    name,
+    username,
+    todos: [],
+  });
+
+  response.status(201).json({
+    id,
+    name,
+    username,
+    todos: [],
+  });
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request; // Recupera o user da requisição
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
